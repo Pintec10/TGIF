e03ceec28cc1b3b13db7f9c118988fa326198692
ROBERTO MILANI <robimilani@gmail.com>
2019-09-04 15:35:20 +0200

    post-presentation, added personalized message when filters return no result

--- javascript/main_Vue.js
+++ javascript/main_Vue.js
@@ -12,7 +12,74 @@ var app = new Vue({
         loaded: false,
         statesArray: [],
         selectedParties: [],
-        selectedState: "all"
+        selectedState: "all",
+        partiesLegend: {
+            "D": "democrat",
+            "R": "republican",
+            "I": "independent"
+        },
+        statesLegend: {
+            "all": "any state",
+            "AL": "Alabama",
+            "AK": "Alaska",
+            "AS": "American Samoa",
+            "AZ": "Arizona",
+            "AR": "Arkansas",
+            "CA": "California",
+            "CO": "Colorado",
+            "CT": "Connecticut",
+            "DE": "Delaware",
+            "DC": "District Of Columbia",
+            "FM": "Federated States Of Micronesia",
+            "FL": "Florida",
+            "GA": "Georgia",
+            "GU": "Guam",
+            "HI": "Hawaii",
+            "ID": "Idaho",
+            "IL": "Illinois",
+            "IN": "Indiana",
+            "IA": "Iowa",
+            "KS": "Kansas",
+            "KY": "Kentucky",
+            "LA": "Louisiana",
+            "ME": "Maine",
+            "MH": "Marshall Islands",
+            "MD": "Maryland",
+            "MA": "Massachusetts",
+            "MI": "Michigan",
+            "MN": "Minnesota",
+            "MS": "Mississippi",
+            "MO": "Missouri",
+            "MT": "Montana",
+            "NE": "Nebraska",
+            "NV": "Nevada",
+            "NH": "New Hampshire",
+            "NJ": "New Jersey",
+            "NM": "New Mexico",
+            "NY": "New York",
+            "NC": "North Carolina",
+            "ND": "North Dakota",
+            "MP": "Northern Mariana Islands",
+            "OH": "Ohio",
+            "OK": "Oklahoma",
+            "OR": "Oregon",
+            "PW": "Palau",
+            "PA": "Pennsylvania",
+            "PR": "Puerto Rico",
+            "RI": "Rhode Island",
+            "SC": "South Carolina",
+            "SD": "South Dakota",
+            "TN": "Tennessee",
+            "TX": "Texas",
+            "UT": "Utah",
+            "VT": "Vermont",
+            "VI": "Virgin Islands",
+            "VA": "Virginia",
+            "WA": "Washington",
+            "WV": "West Virginia",
+            "WI": "Wisconsin",
+            "WY": "Wyoming"
+        }
     },
 
     methods: {
@@ -20,8 +87,8 @@ var app = new Vue({
             return fetch(this.url, this.initObject)
                 .then(function (response) {
                     return response.json()
-                }).then(function (json) {
-                    app.membersArray = json.results[0].members;
+                }).then((json) => {
+                    this.membersArray = json.results[0].members;
                     //adding the "votes_with_party" numerical element (not present in initial data)
                     for (let n in app.membersArray) {
                         app.membersArray[n].votes_with_party = Math.round(app.membersArray[n].total_votes * app.membersArray[n].votes_with_party_pct / 100);
@@ -37,7 +104,7 @@ var app = new Vue({
         buildDropdown() {
             // builds array with states
             for (let i = 0; i < this.membersArray.length; i++) {
-                if (this.statesArray.includes(this.membersArray[i].state)) { } else {
+                if (!this.statesArray.includes(this.membersArray[i].state)) {
                     this.statesArray.push(this.membersArray[i].state);
                 }
             }
@@ -47,7 +114,27 @@ var app = new Vue({
 
     computed: {
         filterData() {
-            return this.membersArray.filter(x => (app.selectedParties.includes(x.party) || app.selectedParties.length === 0) && (app.selectedState.includes(x.state) || app.selectedState === "all"));
+            return this.membersArray.filter(x => {
+                let party = (app.selectedParties.includes(x.party) || app.selectedParties.length === 0);
+                let state = (app.selectedState.includes(x.state) || app.selectedState === "all");
+                return party && state;
+            })
+        },
+
+        selectedFiltersStringBuilder() {
+
+            let selectedPartiesString = "";
+            for (let i = 0; i < this.selectedParties.length; i++) {
+                selectedPartiesString += this.partiesLegend[this.selectedParties[i]];
+                if (i < this.selectedParties.length - 2) {
+                    selectedPartiesString += ", ";
+                }
+                if (i === this.selectedParties.length - 2) {
+                    selectedPartiesString += " or ";
+                }
+            }
+            selectedPartiesString += " delegates from " + this.statesLegend[this.selectedState];
+            return selectedPartiesString;
         }
     },
 
